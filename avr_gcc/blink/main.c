b23d3a6b17ba058a6f830283cd63bd2d28ad91ea
mtr577 <thrinathrao77@gmail.com>
2025-09-17 12:37:21 +0000

    added pdfs

--- avr_gcc/blink/main.c
+++ avr_gcc/blink/main.c
@@ -9,7 +9,7 @@ int main (void)
 	
   /* Arduino boards have a LED at PB5 */
  //set PB5, pin 13 of arduino as output
-  DDRB    |= ((1 << DDB0));
+  DDRB    |= ((1 << DDB5));
   while (1) {
 //turn led off    
     PORTB = ((0 <<  PB0));
